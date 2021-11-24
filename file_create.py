1d975d4405394c46022dd3ea2ec55ddf1dd439c9
Johan <86954289+Wickedviruz@users.noreply.github.com>
2021-11-24 21:06:22 +0100

    Delete file_create.py

--- file_create.py
+++
@@ -1,8 +0,0 @@
-import json
-import slutuppdragv1
-
-class CJSON:
-
-    def create_file():
-        with open("data_file.json", "w") as write_file:
-            json.dump(slutuppdragv1(), write_file)
